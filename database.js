842ea97a1b2e78ef8ca184e3cb5a6983a58c1a72
Personal <personal@Ankits-MacBook-Pro.local>
2025-03-14 12:15:47 +0530

    Added MongoDB Database

---
+++ database.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+console.log("Connect to Database")
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+mongoose.connect("mongodb+srv://ankitgkp:4yCUKIC85OTEsa5o@cluster0.pagrh.mongodb.net/coursera-app")
+
+const userSchema = new Schema({
+    email: {type : String, unique : true},
+    password: String,
+    firstName : String,
+    lastName : String,
+});
+
+const adminSchema = new Schema({
+    email: {type : String, unique : true},
+    password: String,
+    firstName : String,
+    lastName : String,
+});
+
+const courseSchema = new Schema({
+    title: String,
+    description : String,
+    price : Number,
+    imageUrl : String,
+    creatorId: ObjectId
+});
+
+const purchaseSchema = new Schema({
+    userId: ObjectId,
+    courseId: ObjectId,
+
+});
+
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+
+module.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
